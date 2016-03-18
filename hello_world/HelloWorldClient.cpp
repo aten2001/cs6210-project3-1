f48e579bfbd7eddeaa56c57e52f6014ed2a10a3b
Dilan Manatunga <manatunga@gmail.com>
2016-03-18 12:43:45 -0400

    Added argument parsing to client and server

--- hello_world/HelloWorldClient.cpp
+++ hello_world/HelloWorldClient.cpp
@@ -2,29 +2,51 @@
 #include <thrift/protocol/TBinaryProtocol.h>
 #include <thrift/transport/TSocket.h>
 #include <thrift/transport/TTransportUtils.h>
+#include <boost/program_options.hpp>
 
 #include "gen-cpp/HelloWorld.h"
 
-using namespace std;
 using namespace apache::thrift;
 using namespace apache::thrift::protocol;
 using namespace apache::thrift::transport;
+namespace po = boost::program_options;
 
 using namespace helloworld;
 
-int main() {
-  boost::shared_ptr<TTransport> socket(new TSocket("localhost", 9090));
+int main(int argc, char** argv) {
+  std::string host;
+  int port;
+  po::options_description desc("Options");
+  desc.add_options()
+          ("help,h", "Produce help message")
+          ("host", po::value<std::string>(&host)->default_value("localhost"), "Hostname")
+          ("port,p", po::value<int>(&port)->default_value(9090), "Port")
+          ;
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);
+
+  if (vm.count("help")) {
+    std::cout << desc << std::endl;
+    return EXIT_SUCCESS;
+  }
+
+  boost::shared_ptr<TTransport> socket(new TSocket(host, port));
   boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
   boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
 
   HelloWorldClient client(protocol);
 
+  std::cout << "Connecting to server " << host << ":" << port << std::endl;
   try {
     transport->open();
-    string time;
+    std::string time;
+    std::cout << "Calling RPC method get_time" << std::endl;
     client.get_time(time);
-    cout << time << endl;
+    std::cout << time;
+    std::cout << "Finished!" << std::endl;
   } catch (TException& tx) {
-    cout << "ERROR: " << tx.what() << endl;
+    std::cout << "ERROR: " << tx.what() << std::endl;
   }
 }
--- hello_world/HelloWorldServer.cpp
+++ hello_world/HelloWorldServer.cpp
@@ -3,6 +3,7 @@
 #include <thrift/server/TSimpleServer.h>
 #include <thrift/transport/TServerSocket.h>
 #include <thrift/transport/TBufferTransports.h>
+#include <boost/program_options.hpp>
 
 using namespace ::apache::thrift;
 using namespace ::apache::thrift::protocol;
@@ -10,8 +11,10 @@ using namespace ::apache::thrift::transport;
 using namespace ::apache::thrift::server;
 
 using boost::shared_ptr;
+namespace po = boost::program_options;
 
-using namespace  ::helloworld;
+using namespace ::helloworld;
+
 
 class HelloWorldHandler : virtual public HelloWorldIf {
 public:
@@ -23,13 +26,29 @@ public:
     std::cout << "get_time called" << std::endl;
     time_t timer;
     time(&timer);
-    _return = ctime(&timer);
+    _return.assign(ctime(&timer));
   }
 
 };
 
-int main(int argc, char **argv) {
-  int port = 9090;
+int main(int argc, char** argv) {
+  int port;
+  po::options_description desc("Options");
+  desc.add_options()
+          ("help,h", "Produce help message")
+          ("port,p", po::value<int>(&port)->default_value(9090), "Server Listening Port")
+          ;
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);
+
+  if (vm.count("help")) {
+    std::cout << desc << std::endl;
+    return EXIT_SUCCESS;
+  }
+
+  std::cout << "Starting Server: Listening on Port " << port << std::endl;
   shared_ptr<HelloWorldHandler> handler(new HelloWorldHandler());
   shared_ptr<TProcessor> processor(new HelloWorldProcessor(handler));
   shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
@@ -38,5 +57,5 @@ int main(int argc, char **argv) {
 
   TSimpleServer server(processor, serverTransport, transportFactory, protocolFactory);
   server.serve();
-  return 0;
+  return EXIT_SUCCESS;
 }
