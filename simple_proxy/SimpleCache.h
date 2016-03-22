ecf05e823adbfcbe80c18b95d1ef06e9ee6776c4
Pranith Kumar <bobby.prani@gmail.com>
2016-03-22 14:58:53 -0400

    Track size of the cache

--- simple_proxy/SimpleCache.h
+++ simple_proxy/SimpleCache.h
@@ -10,18 +10,20 @@
 
 class SimpleCache {
 public:
-        SimpleCache() {}
-	SimpleCache(CacheRepPolicy &pol, size_t cache_size);
-	~SimpleCache() {}
+  SimpleCache() {}
+  SimpleCache(CacheRepPolicy &pol, size_t cache_size);
+  ~SimpleCache() {}
 
-	bool is_url_cached(const std::string& url);
-	std::string get_url_cached(const std::string& url);
-	void cache_url(const std::string& _return, const std::string& url);
+  bool is_url_cached(const std::string& url);
+  std::string get_url_cached(const std::string& url);
+  void cache_url(const std::string& _return, const std::string& url);
+  void free_cache(size_t size);
+
 private:
-	size_t         m_max_cache_size;
-	size_t         m_cur_cache_size;	
-	CacheRepPolicy m_rep_policy;
-	std::unordered_map<size_t, std::string> m_cache;
+  size_t         m_max_cache_size;
+  size_t         m_cur_cache_size;	
+  CacheRepPolicy m_rep_policy;
+  std::unordered_map<size_t, std::string> m_cache;
 };
 
 #endif /* _SIMPLE_CACHE_H */
