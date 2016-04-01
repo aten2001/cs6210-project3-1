d10ff7a5b5a2b94be4e0fa4f1b18cd17af1f4e25
Pranith Kumar <bobby.prani@gmail.com>
2016-04-01 17:36:13 -0400

    update parse.sh

--- caching_proxy/parse.sh
+++ caching_proxy/parse.sh
@@ -3,8 +3,8 @@
 workloads=( random size )
 for workload in "${workloads[@]}"
 do
-    grep Items ${workload}_results.txt | awk '{ print $4; }' >> ${workload}_pages.txt
-    grep Occupancy ${workload}_results.txt | awk '{ print $4; }' >> ${workload}_occ.txt
-    grep Hit ${workload}_results.txt | awk '{ print $3; }' >> ${workload}_hit.txt
-    grep Time ${workload}_results.txt | awk '{ print $3; }' >> ${workload}_time.txt
+    grep Items ${workload}_results.txt | awk '{ print $4; }' | xargs -n7 >> ${workload}_pages.txt
+    grep Occupancy ${workload}_results.txt | awk '{ print $4; }' | xargs -n7 >> ${workload}_occ.txt
+    grep Hit ${workload}_results.txt | awk '{ print $3; }' | xargs -n7 >> ${workload}_hit.txt
+    grep Time ${workload}_results.txt | awk '{ print $3; }' | xargs -n7 >> ${workload}_time.txt
 done
